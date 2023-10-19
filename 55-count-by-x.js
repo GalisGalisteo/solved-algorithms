b84a38a18044307405cc5f39630fbee730030cc1
GalisGalisteo <galis.travelling@gmail.com>
2023-10-19 08:25:57 +0200

    ex 55 finished

--- 55-count-by-x.js
+++ 55-count-by-x.js
@@ -12,10 +12,16 @@
 
 function countBy(x, n) {
   let z = [];
-
+  for (let i = x; i <= n * x; i += x) {
+    z.push(i);
+  }
   return z;
 }
 
 console.log(countBy(1, 10)); //  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 
 console.log(countBy(2, 5)); // [2, 4, 6, 8, 10]
+
+console.log(countBy(3, 6));
+
+console.log(countBy(10, 20));
