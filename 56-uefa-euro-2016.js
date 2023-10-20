60c9a8885aea8fdd4dd5fd7ec9c893dc637fec16
GalisGalisteo <galis.travelling@gmail.com>
2023-10-20 08:53:47 +0200

    ex 56 finished

--- 56-uefa-euro-2016.js
+++ 56-uefa-euro-2016.js
@@ -9,9 +9,17 @@
  */
 
 function uefaEuro2016(teams, scores) {
-  // your code...
+  let result;
+  if (scores[0] > scores[1]) {
+    result = `${teams[0]} won!`
+  } else if (scores[0] < scores[1]) {
+    result = `${teams[1]} won!`
+  } else {
+    result = `teams played draw.`
+  }
+  return `At match ${teams[0]} - ${teams[1]}, ${result}`
 }
 
-uefaEuro2016(["Germany", "Ukraine"], [2, 0]); // "At match Germany - Ukraine, Germany won!"
-uefaEuro2016(["Belgium", "Italy"], [0, 2]); // "At match Belgium - Italy, Italy won!"
-uefaEuro2016(["Portugal", "Iceland"], [1, 1]); // "At match Portugal - Iceland, teams played draw."
+console.log(uefaEuro2016(["Germany", "Ukraine"], [2, 0])); // "At match Germany - Ukraine, Germany won!"
+console.log(uefaEuro2016(["Belgium", "Italy"], [0, 2])); // "At match Belgium - Italy, Italy won!"
+console.log(uefaEuro2016(["Portugal", "Iceland"], [1, 1])); // "At match Portugal - Iceland, teams played draw."
